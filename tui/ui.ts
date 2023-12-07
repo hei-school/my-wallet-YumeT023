31650e01570ca45fd8e3ed914f04f6774cffe1bd
Yume <hei.hajatiana@gmail.com>
2023-12-07 09:26:23 +0300

    feat: improve terminal ui

--- tui/ui.ts
+++ tui/ui.ts
@@ -19,18 +19,54 @@ export const LOGO = () => {
 };
 
 export const ask = <T extends string | number | undefined>(
-  str: string,
+  question: string,
   defaultValue?: T,
 ) => {
-  return prompt(colors.yellow("> ") + str + "\n", String(defaultValue || ""));
+  return prompt(
+    `${colors.yellow("> ")}${question}\n`,
+    String(defaultValue || ""),
+  ) as T;
 };
 
-export const listMenu = (...items: string[]) => {
+export const askInt = (str: string, err: string, defaultValue = 0) => {
+  const answer = +ask(str, defaultValue);
+  if (isNaN(answer)) {
+    console.log(colors.brightRed(err));
+    return defaultValue;
+  }
+  return answer;
+};
+
+export const askStr = (
+  str: string,
+  err: string,
+  defaultValue?: string,
+): string => {
+  const answer = ask<string>(str, defaultValue);
+  if (answer && !/[a-zA-Z].*/.test(answer)) {
+    console.log(colors.brightRed(err));
+    return askStr(str, err, defaultValue);
+  }
+  return answer;
+};
+
+export const printNumberedList = (items: string[]) => {
   const decimal = items.map((str, index) =>
     `${colors.yellow(String(index + 1))} - ${str}`
   ).join("\n");
   console.log(decimal);
-  br();
+  printNewline();
 };
 
-export const br = (times = 1) => console.log("\n".repeat(times));
+export const printNewline = (times = 1) => console.log("\n".repeat(times));
+
+export const printObject = (
+  header: string,
+  object: Record<string, unknown>,
+) => {
+  console.log(colors.yellow(header));
+  console.log("------------------");
+  Object.keys(object).forEach((key) => {
+    console.log(colors.yellow(key), object[key]);
+  });
+};
