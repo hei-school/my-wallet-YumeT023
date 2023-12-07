b693aa832ee81f6e0a98c0527ee255868368dc34
Yume <hei.hajatiana@gmail.com>
2023-12-07 09:25:45 +0300

    feat: add color variant

--- tui/colors.ts
+++ tui/colors.ts
@@ -1,3 +1,7 @@
 import { colors } from "../deps.ts";
 
 export const question = (str: string) => colors.blue(str);
+
+export const confirmation = (str: string) => colors.yellow(str);
+
+export const money = (str: string) => colors.green(str);
