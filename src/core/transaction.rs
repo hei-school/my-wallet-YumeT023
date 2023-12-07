c5a4383d869d9b68b051b996a58a068464446a9d
Yume <hei.hajatiana@gmail.com>
2023-12-07 09:59:35 +0300

    chore: add transaction

---
+++ src/core/transaction.rs
@@ -0,0 +1,30 @@
+use std::io::Error;
+
+pub enum TransactionType {
+    DEPOSIT,
+    WITHDRAWAL,
+}
+
+pub struct Transaction {
+    pub ttype: TransactionType,
+    pub amount: u16,
+}
+
+impl Transaction {
+    pub fn new(ttype: TransactionType, amount: u16) -> Transaction {
+        Self { ttype, amount }
+    }
+
+    pub fn deposit(amount: u16) -> Transaction {
+        Self::new(TransactionType::DEPOSIT, amount)
+    }
+
+    pub fn withdraw(amount: u16) -> Transaction {
+        Self::new(TransactionType::WITHDRAWAL, amount)
+    }
+}
+
+pub trait Transactionable {
+    fn deposit(&mut self, amount: u16) -> Transaction;
+    fn withdraw(&mut self, amount: u16) -> Result<Transaction, Error>;
+}
