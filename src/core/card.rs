2a0ea4dbc42a5488791bea80d9f6d5aca885f1fe
Yume <hei.hajatiana@gmail.com>
2023-12-09 18:43:56 +0300

    chore: impl CardPocket for Wallet

--- src/core/card.rs
+++ src/core/card.rs
@@ -1,29 +1,37 @@
-use std::fmt::Display;
+use std::{fmt::Display, io::Error};
 
 use super::sized::Sized;
 
+#[derive(Clone)]
 pub struct Owner(String);
 
-pub enum Cards {
+#[derive(Clone)]
+pub enum Card {
     Bank(Owner),
     National(Owner),
     DrivingLicense(Owner),
 }
 
-impl Display for Cards {
+pub trait CardPocket<IDX> {
+    fn get_card(&self, idx: IDX) -> Option<&Card>;
+    fn put_card(&mut self, to_put: Card) -> Result<(), Error>;
+    fn get_all(&self) -> Vec<Card>;
+}
+
+impl Display for Card {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!(
             "{}",
             match self {
-                Cards::Bank(owner) => format!("{}:{}", "Bank card", owner.0),
-                Cards::DrivingLicense(owner) => format!("{}:{}", "Driving license", owner.0),
-                Cards::National(owner) => format!("{}:{}", "National card", owner.0),
+                Card::Bank(owner) => format!("{}:{}", "Bank card", owner.0),
+                Card::DrivingLicense(owner) => format!("{}:{}", "Driving license", owner.0),
+                Card::National(owner) => format!("{}:{}", "National card", owner.0),
             }
         ))
     }
 }
 
-impl Sized for Cards {
+impl Sized for Card {
     fn compute_size(&self) -> f32 {
         1.0
     }
--- src/core/wallet.rs
+++ src/core/wallet.rs
@@ -1,20 +1,26 @@
+use std::io::{Error, ErrorKind::InvalidData};
+
 use super::{
     action::Action,
     money::Money,
-    transaction::{Transaction, Transactional},
+    transaction::{Transaction, Transactional}, sized::Sized, card::{Card, CardPocket},
 };
 
 pub struct Wallet {
     pub owner: String,
     pub action_history: Vec<Action>,
+    cards: Vec<Card>,
     balance: Money,
 }
+
+const WALLET_DEFAULT_SIZE: f32 = 50.0;
 
 impl Wallet {
     pub fn new(owner: &str) -> Wallet {
         Self {
             owner: owner.to_string(),
             balance: Money::from(0.0),
+            cards: vec![],
             action_history: vec![],
         }
     }
@@ -26,6 +32,18 @@ impl Wallet {
     pub fn queue_history(&mut self, action: Action) -> Action {
         self.action_history.push(action.clone());
         action
+    }
+
+    pub fn compute_available_space(&self) -> f32 {
+        WALLET_DEFAULT_SIZE - self.compute_size()
+    }
+
+    pub fn assert_sized_fits(&self, sized: &impl Sized) -> Result<(), Error> {
+        let size = sized.compute_size();
+        if self.compute_available_space() < size {
+            return Err(Error::new(InvalidData, format!("No more space for {}", size)))
+        }
+        Ok(())
     }
 }
 
@@ -41,5 +59,29 @@ impl Transactional for Wallet {
         let transaction = self.balance.withdraw(amount)?;
         self.queue_history(Action::Transaction(transaction.clone()));
         Ok(transaction)
+    }
+}
+
+impl CardPocket<usize> for Wallet {
+    fn put_card(&mut self, to_add: Card) -> Result<(), std::io::Error> {
+        self.assert_sized_fits(&to_add)?;
+        self.cards.push(to_add);
+        Ok(())
+    }
+
+    fn get_all(&self) -> Vec<Card> {
+        self.cards.clone()
+    }
+
+    fn get_card(&self, idx: usize) -> Option<&Card> {
+        self.cards.get(idx - 1)
+    }
+}
+
+impl Sized for Wallet {
+    fn compute_size(&self) -> f32 {
+        let card_size = self.cards.iter().map(|x| x.compute_size()).sum::<f32>();
+        let balance_size = self.balance.compute_size();
+        card_size + balance_size
     }
 }
